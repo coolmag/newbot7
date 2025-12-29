0be341358430a1937409779858277c219df1fe97
coolmag <54004066+coolmag@users.noreply.github.com>
2025-12-29 19:33:15 +0300

    Add files via upload

--- webapp/service-worker.js
+++ webapp/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'aurora-player-v5'; // V5
+const CACHE_NAME = 'aurora-player-v6';
 const ASSETS = [
     './', './index.html', './style.css',
     './js/main.js', './js/api.js', './js/player.js',
